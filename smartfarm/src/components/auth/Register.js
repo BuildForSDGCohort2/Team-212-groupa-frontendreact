44ff57127eafba1efe6763749991c0efcb1ecdc1
Paul Wamaria <paulwamaria@gmail.com>
2020-10-01 19:59:51 +0300

    Add other pages templates.

---
+++ smartfarm/src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Register extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text-success">The registration form is being updated, please come back later</h1>
+            </div>
+        )
+    }
+}
+
+export default Register
