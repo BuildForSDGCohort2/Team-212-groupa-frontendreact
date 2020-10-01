7f3df60ed039d2b16e30d7024a16355eb42c7e1c
Paul Wamaria <paulwamaria@gmail.com>
2020-10-01 13:20:52 +0300

    Set up the frontend project files using create-react-app

---
+++ smartfarm/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-fluid text-center">
+        <hi className="text-info"> Welcome to smartfarm</hi>
+      </div>
+    </div>
+  );
+}
+
+export default App;
