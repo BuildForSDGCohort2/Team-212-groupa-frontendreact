9bdab191a894fab81395f41e105de5b0284f5579
Paul Wamaria <paulwamaria@gmail.com>
2020-10-02 22:47:20 +0300

    Add Login Form

--- smartfarm/src/components/auth/Login.js
+++ smartfarm/src/components/auth/Login.js
@@ -1,13 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Login extends Component {
-    render() {
-        return (
-            <div>
-                <h1 className="text-success">The Login form is being updated</h1>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "username",
+      password: "password",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <form className="mt-5 mb-5">
+              <legend className="text-success">Sign In</legend>
+              <div>
+                <label htmlFor="usernameReg">Username</label>
+              </div>
+              <div>
+                <input
+                  className="form-control"
+                  type="text"
+                  id="usernameReg"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="passwordReg">Password</label>
+              </div>
+              <div>
+                <input
+                  className="form-control"
+                  type="text"
+                  id="passwordReg"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <button className="mt-3" type="submit">
+                Submit
+              </button>
+              <p className="mt-2">
+                Don't have an account? <Link to="/register">Register</Link>
+              </p>
+            </form>
+          </div>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
