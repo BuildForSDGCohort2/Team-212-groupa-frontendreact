0b81b38f4e616c1fbe0166e36cbaa81538c387a1
PAUL WAMARIA <52450220+Paulwamaria@users.noreply.github.com>
2020-10-02 13:00:29 +0300

    Update Navbar.js

--- smartfarm/src/components/layout/Navbar.js
+++ smartfarm/src/components/layout/Navbar.js
@@ -18,8 +18,6 @@ class Mynavbar extends React.Component {
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/register">Register</Nav.Link>
-                    <Nav.Link href="/login">Login</Nav.Link>
                     <Nav.Link href="/arcticles">Arcticles</Nav.Link>
                   </Nav>
 
