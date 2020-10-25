c1c96599957d6fdf3912367e3b2732a35500edce
Paul Wamaria <paulwamaria@gmail.com>
2020-10-25 15:34:05 +0300

    Add articles reducer to the main reducer

--- smartfarm/src/reducers/index.js
+++ smartfarm/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import auth from "./auth";
 import errorReducer from "./errors";
 import messageReducer from "./messages";
+import articles from "./articles";
 
 const rootReducer = combineReducers({
   auth,
   errorReducer,
   messageReducer,
+  articles,
 });
 
 export default rootReducer;
