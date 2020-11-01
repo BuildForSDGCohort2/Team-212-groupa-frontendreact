6be578cb8a2a06547cda69bde5b92e8fd5c377ea
Paul Wamaria <paulwamaria@gmail.com>
2020-11-01 20:29:54 +0300

    Add article detail constants

--- smartfarm/src/actions/articles.js
+++ smartfarm/src/actions/articles.js
@@ -6,6 +6,9 @@ import {
   ADD_ARTICLES_FAIL,
   ADD_ARTICLES_SUCCESS,
   ADD_ARTICLES_REQUEST,
+  ARTICLE_LOADING,
+  ARTICLE_LOADED,
+  FETCH_ARTICLE_FAIL,
 } from "../types/types";
 import axios from "axios";
 // import { createMessage } from "./messages";
@@ -18,20 +21,25 @@ export const loadArticles = (stage) => async (dispatch, getState) => {
   //Get token from state
   const token = getState().auth.token;
 
+  const data = JSON.stringify({ stage });
+
   let config = {
-    method: "get",
-    url: `https://smartfarmendpoints.herokuapp.com/api/blog/articles/${stage}`,
+    method: "post",
+    url: `https://smartfarmendpoints.herokuapp.com/api/blog/articles/stage`,
     headers: {
       "Content-Type": "application/json",
     },
+    data: data,
   };
 
   //if token, add to headers config
   if (token) {
     config.headers["Authorization"] = `Token ${token}`;
   }
+  console.log(config);
   try {
     const response = await axios(config);
+    console.log(response);
     dispatch({
       type: GET_ARTICLES_SUCCESS,
       payload: response.data,
@@ -54,7 +62,7 @@ export const loadAllArticles = () => async (dispatch) => {
   });
   try {
     const response = await axios.get(
-      `https://smartfarmendpoints.herokuapp.com/api/blog/article`
+      `https://smartfarmendpoints.herokuapp.com/api/blog/articles`
     );
     dispatch({
       type: GET_ARTICLES_SUCCESS,
@@ -69,6 +77,32 @@ export const loadAllArticles = () => async (dispatch) => {
     dispatch({
       type: GET_ARTICLES_FAIL,
     });
+  }
+};
+
+export const loadArticle = (slug) => async (dispatch) => {
+  dispatch({ type: ARTICLE_LOADING });
+  let config = {
+    method: "get",
+    url: `https://smartfarmendpoints.herokuapp.com/api/blog/article/detail/${slug}`,
+  };
+
+  try {
+    const response = await axios(config);
+    dispatch({
+      type: ARTICLE_LOADED,
+      payload: response.data,
+    });
+  } catch (err) {
+    const errors = {
+      msg: err.response.data,
+      status: err.response.status,
+    };
+    dispatch({
+      type: GET_ERRORS,
+      payload: errors,
+    });
+    dispatch({ type: FETCH_ARTICLE_FAIL });
   }
 };
 
@@ -113,7 +147,7 @@ export const addArticles = (crop, farmer, title, stage, content) => (
         payload: errors,
       });
       dispatch({
-        type:ADD_ARTICLES_FAIL
-      })
+        type: ADD_ARTICLES_FAIL,
+      });
     });
 };
